a424a47cfa255ff5664d9309532a820b83e7281d
Edmondo Porcu <edmondo.porcu@gmail.com>
2023-06-04 17:17:10 -0700

    Chapter3

---
+++ chapter3/participants.go
@@ -0,0 +1,101 @@
+package chapter3
+
+type Team struct {
+	Name string
+}
+
+type Pot struct {
+	Teams []Team
+}
+
+type Participants struct {
+	Pots []Pot
+}
+
+func (p *Participants) TeamCount() int {
+	count := 0
+	for _, pot := range p.Pots {
+		count += len(pot.Teams)
+	}
+	return count
+}
+
+type Pairing struct {
+	Left  Team
+	Right Team
+}
+
+type Result struct {
+	Winner Team
+}
+
+func Pot1() Pot {
+	return Pot{
+		Teams: []Team{
+			{Name: "Real Madrid"},
+			{Name: "Eintracht Frankfurt"},
+			{Name: "Manchester City"},
+			{Name: "AC Milan"},
+			{Name: "Bayern Munich"},
+			{Name: "Paris Saint-Germain"},
+			{Name: "FC Porto"},
+			{Name: "Ajax"},
+		},
+	}
+}
+
+func Pot2() Pot {
+	return Pot{
+		[]Team{
+			{Name: "Liverpool"},
+			{Name: "Chelsea"},
+			{Name: "Barcelona"},
+			{Name: "Juventus"},
+			{Name: "Atletico Madrid"},
+			{Name: "Sevilla"},
+			{Name: "RB Leipzig"},
+			{Name: "Tottenham Hotspur"},
+		},
+	}
+}
+
+func Pot3() Pot {
+	return Pot{
+		[]Team{
+			{Name: "Borussia Dortmund"},
+			{Name: "FC Salzburg"},
+			{Name: "Shakhtar Donetsk"},
+			{Name: "Inter Milan"},
+			{Name: "Napoli"},
+			{Name: "Benfica"},
+			{Name: "Sporting CP"},
+			{Name: "Bayer Leverkusen"},
+		},
+	}
+}
+
+func Pot4() Pot {
+	return Pot{
+		[]Team{
+			{Name: "Marseille"},
+			{Name: "Brugge"},
+			{Name: "Celtic"},
+			{Name: "Viktoria Plzen"},
+			{Name: "Maccabi Haifa"},
+			{Name: "Rangers"},
+			{Name: "FC Copenhagen"},
+			{Name: "Dinamo Zagreb"},
+		},
+	}
+}
+
+func GetParticipants() Participants {
+	return Participants{
+		Pots: []Pot{
+			Pot1(),
+			Pot2(),
+			Pot3(),
+			Pot4(),
+		},
+	}
+}
